5c53f615952f56aa017332f1d5108637a817ed5b
Jesús Avelar <wisusdev@gmail.com>
2024-05-19 18:12:52 -0600

    feat: Add route for file processing

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\ProcessController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::post('/process/file', [ProcessController::class, 'processFile'])->name('process.file');
